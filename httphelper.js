7f50125f99997a147566f3186a160204dcdd853e
keepoffthegrass <rick.walsh@gmail.com>
2010-09-14 11:20:12 +0100

    now module.export an object from each helper

--- httphelper.js
+++ httphelper.js
@@ -1,44 +1,47 @@
 
-function () {
+module.exports = function () {
 
-	exports.getRequestBuilder = function(host, port, url, callback) {
-		var request = http.createClient(port, host);
-		request = http.createClient(port, host).request("GET", url, {"Host": host});
-		request.addListener('response', function (response) {
-			var result= "";
-			response.addListener("data", function (chunk) {
-			  result+= chunk;
+	return {
+
+		getRequestBuilder: function(host, port, url, callback) {
+			var request = http.createClient(port, host);
+			request = http.createClient(port, host).request("GET", url, {"Host": host});
+			request.addListener('response', function (response) {
+				var result= "";
+				response.addListener("data", function (chunk) {
+				  result+= chunk;
+				});
+				response.addListener("end", function () {  
+					response.body = result;
+					callback(null, response);
+				}); 
 			});
-			response.addListener("end", function () {  
-				response.body = result;
-				callback(null, response);
-			}); 
-		});
-		request.end();
-	}
+			request.end();
+		},
 
-	exports.postRequestBuilder = function(host, port, url, content, callback) {
+		postRequestBuilder: function(host, port, url, content, callback) {
 
-		var request = http.createClient(port, host).request("POST", url, {"Host": host, "Content-Length": content.length});
-		request.write(content);
-		
-		request.addListener('response', function (response) {
-			var result= "";
-			response.addListener("data", function (chunk) {
-			  result+= chunk;
+			var request = http.createClient(port, host).request("POST", url, {"Host": host, "Content-Length": content.length});
+			request.write(content);
+			
+			request.addListener('response', function (response) {
+				var result= "";
+				response.addListener("data", function (chunk) {
+				  result+= chunk;
+				});
+				response.addListener("end", function () {  
+					response.body = result;
+					callback(null, response);
+				}); 
 			});
-			response.addListener("end", function () {  
-				response.body = result;
-				callback(null, response);
-			}); 
-		});
-		request.end();
-	}
+			request.end();
+		},
 
-	exports.assertStatus = function(code) {
-		return function (e, res) {
-			assert.equal (res.statusCode, code);
-		};
+		exports.assertStatus: function(code) {
+			return function (e, res) {
+				assert.equal (res.statusCode, code);
+			};
+		}
 	}
 
 }
--- mongohelper.js
+++ mongohelper.js
@@ -1,58 +1,58 @@
 
 
-function MongoHelper(Db, Server, ObjectID) {
+module.exports = function MongoHelper(Db, Server, ObjectID) {
 
-	exports.createPrimaryKey = function() {
-		return ObjectID.createPk();
-	}
+	return {
 
-	function removeDataFromCollection(host, port, database, collectionName, closeConnectionAfterRemoval, callback) {
-		var db = new Db(database, new Server(host, port, {auto_reconnect: true}, {strict:true}));
-		db.open(function(){});
-		db.collection(collectionName, function(error, collection) {
-			 if(error) {
-				db.close();
-				callback(error);
-			 } else {
-				 collection.remove(function(error,result) {
-					if(closeConnectionAfterRemoval) db.close();
-					callback(error, result);
-				 });
-			 }
+		createPrimaryKey:  function() {
+			return ObjectID.createPk();
+		},
 
-		})
-	};
+		removeDataFromCollection: function (host, port, database, collectionName, closeConnectionAfterRemoval, callback) {
+			var db = new Db(database, new Server(host, port, {auto_reconnect: true}, {strict:true}));
+			db.open(function(){});
+			db.collection(collectionName, function(error, collection) {
+				 if(error) {
+					db.close();
+					callback(error);
+				 } else {
+					 collection.remove(function(error,result) {
+						if(closeConnectionAfterRemoval) db.close();
+						callback(error, result);
+					 });
+				 }
 
-	exports.removeDataFromCollection = removeDataFromCollection; 
+			})
+		},
 
+		/**
+		* Removes data from the collection and then insert the provided data.
+		*/
+		addTestDataToMongo: function(host, port, database, collectionName, dataConstructor, callback) {
 
-	/**
-	* Removes data from the collection and then insert the provided data.
-	*/
-	exports.addTestDataToMongo = function(host, port, database, collectionName, dataConstructor, callback) {
-
-		var db = new Db(database, new Server(host, port, {auto_reconnect: true}, {strict:true}));
-		db.open(function(){});
-		db.collection(collectionName, function(error, collection) {
+			var db = new Db(database, new Server(host, port, {auto_reconnect: true}, {strict:true}));
+			db.open(function(){});
+			db.collection(collectionName, function(error, collection) {
+				
+				if(error) {
+					db.close();
+					callback(error,null);
+				} else {
+					var data = dataConstructor();
+					collection.insert(data, function(error, result) {
+						if(error) {
+							db.close();
+							callback(error,null);
+						} else {
+							db.close();
+							callback(null,result);
+						}
+					});
+				}
+			});
 			
-			if(error) {
-				db.close();
-				callback(error,null);
-			} else {
-				var data = dataConstructor();
-				collection.insert(data, function(error, result) {
-					if(error) {
-						db.close();
-						callback(error,null);
-					} else {
-						db.close();
-						callback(null,result);
-					}
-				});
-			}
-		});
-		
-	};
+		}
+	}
 
 }
 
